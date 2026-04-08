39fc700a86cafdf4a46bbbd2e47027dc896ae984
martinez <gema.martinez.sanchez@student.howest.be>
2026-04-08 17:13:13 +0200

    Fix the small bounding boxes problem

--- 20260402_YOLO_SAHI_Training.py
+++ 20260402_YOLO_SAHI_Training.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
 
-import ultralytics
-from sahi.predict import predict
 
-predict(
+from sahi.predict import get_sliced_prediction
+from sahi.auto_model import AutoDetectionModel
+import os
+import cv2
+
+# cargar modelo
+detection_model = AutoDetectionModel.from_pretrained(
     model_type="ultralytics",
-    model_path="/home/martinez/flower_phenotyping/data/runs/segment/train/weights/best.pt",
-    model_confidence_threshold=0.3,
-    source="/home/martinez/flower_phenotyping/Series04",
-    slice_height=640,
-    slice_width=640,
-    overlap_height_ratio=0.2,
-    overlap_width_ratio=0.2
+    model_path="/home/martinez/flower_phenotyping/data/annotations/YOLO/runs/train_plant_seg/weights/best.pt",
+    confidence_threshold=0.4,
+)
+
+# parámetros
+min_area = 4000
+input_dir = "/home/martinez/flower_phenotyping/Series04"
+output_dir = "outputs/Series04_results"
+os.makedirs(output_dir, exist_ok=True)
+
+# procesar imágenes una a una
+for img_name in os.listdir(input_dir):
+    img_path = os.path.join(input_dir, img_name)
+
+    result = get_sliced_prediction(
+        img_path,
+        detection_model,
+        slice_height=960,
+        slice_width=960,
+        overlap_height_ratio=0.2,
+        overlap_width_ratio=0.2,
+        postprocess_type="NMM",
+        postprocess_match_metric="IOS",
+        postprocess_match_threshold=0.2,
+        postprocess_class_agnostic=False,
     )
 
+    # 🔥 FILTRO por área
+    result.object_prediction_list = [
+        pred for pred in result.object_prediction_list
+        if pred.bbox.area >= min_area
+    ]
 
-from sahi.postprocess.combine import NMMPostprocess
-
-result = get_sliced_prediction(
-    image="imagen.jpg",
-    detection_model=detection_model,
-    slice_height=640,
-    slice_width=640,
-    overlap_height_ratio=0.2,
-    overlap_width_ratio=0.2,
-    postprocess_type="NMM",          # Non-Maximum Merging (más conservador que NMS)
-    postprocess_match_metric="IOS",  # Intersection over Smaller area
-    postprocess_match_threshold=0.5,
-    postprocess_class_agnostic=False, # respetar clases al fusionar
-    export_dir="outputs/",
-    export_visual=True
-)
+    # exportar visualización
+    base_name = os.path.splitext(img_name)[0]
+    result.export_visuals(export_dir=output_dir,file_name=f"{base_name}_pred")
