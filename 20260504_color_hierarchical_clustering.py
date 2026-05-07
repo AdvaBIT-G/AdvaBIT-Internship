8f4ed5961306f3ec55532bcbb205180294ff4d88
martinez <gema.martinez.sanchez@student.howest.be>
2026-05-07 16:45:54 +0200

    Final version hierarchical clustering with HSV and 500 pixels extracted

--- 20260504_color_hierarchical_clustering.py
+++ 20260504_color_hierarchical_clustering.py
@@ -8,39 +8,39 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 # =========================
 # 1. LOAD DATA
 # =========================
-df = pd.read_csv("/home/martinez/flower_phenotyping/data/annotations/YOLO_annotations/20260507_color_features.csv")
+df = pd.read_csv("/home/martinez/flower_phenotyping/data/annotations/YOLO_annotations/20260505_color_features.csv")
 
 # =========================
 # 2. SELECT FEATURES
 # =========================
 features = [
-    'median_l', 'median_a', 'median_b_lab',
-    'std_l', 'std_a', 'std_b_lab'
+    'median_h', 'median_s', 'median_v',
+    'std_h', 'std_s', 'std_v'
 ]
 
 X = df[features].values
 # =========================
 # 3. HANDLE HUE CORRECTLY
 # =========================
-# OpenCV: H ∈ [0,179] → convertir a grados reales (0–360)
-#H_deg = X[:, 0] * 2
+#OpenCV: H ∈ [0,179] → convert to real grades (0–360)
+H_deg = X[:, 0] * 2
 
-#H_rad = np.deg2rad(H_deg)
-#H_sin = np.sin(H_rad)
-#H_cos = np.cos(H_rad)
+H_rad = np.deg2rad(H_deg)
+H_sin = np.sin(H_rad)
+H_cos = np.cos(H_rad)
 
-# reconstruir feature matrix sin H original
-#X_transformed = np.column_stack((
-    #H_sin,
-    #H_cos,
-    #X[:, 1:]   # resto de features
-#))
+# build feature matrix without original H
+X_transformed = np.column_stack((
+    H_sin,
+    H_cos,
+    X[:, 1:]   # rest of features
+))
 
 # =========================
 # 4. SCALE
 # =========================
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(X_transformed)
 
 # =========================
 # 5. CLUSTERING
@@ -55,7 +55,7 @@ dendrogram(Z)
 plt.title("Dendrogram")
 plt.xlabel("Samples")
 plt.ylabel("Distance")
-plt.savefig("/home/martinez/flower_phenotyping/results/figures/20260507_color_dendrogram2.png")
+plt.savefig("/home/martinez/flower_phenotyping/results/figures/20260507_color_dendrogram.png")
 plt.close()
 
 # =========================
@@ -84,7 +84,7 @@ plt.plot(K_range, wss, marker='o')
 plt.title("Elbow Method")
 plt.xlabel("k")
 plt.ylabel("WSS")
-plt.savefig("/home/martinez/flower_phenotyping/results/figures/20260507_elbow_plot2.png")
+plt.savefig("/home/martinez/flower_phenotyping/results/figures/20260507_elbow_plot.png")
 plt.close()
 
 # =========================
@@ -99,8 +99,8 @@ clusters = fcluster(Z, k_opt, criterion='maxclust')
 plt.figure(figsize=(8, 6))
 
 scatter = plt.scatter(
-    df['median_a'],
-    df['median_b_lab'],
+    df['median_h'],
+    df['median_v'],
     c=clusters,
     cmap='tab10',
     alpha=0.8
@@ -114,13 +114,23 @@ plt.axhline(0, color='gray', linestyle='--', linewidth=1)
 plt.axvline(0, color='gray', linestyle='--', linewidth=1)
 plt.colorbar(scatter)
 
-plt.savefig("/home/martinez/flower_phenotyping/results/figures/cielab_clusters2.png", dpi=300)
+plt.savefig("/home/martinez/flower_phenotyping/results/figures/hsv_clusters.png", dpi=300)
 plt.close()
 
 # =========================
 # 10. SAVE RESULTS
 # =========================
 df['cluster'] = clusters
-df.to_csv("/home/martinez/flower_phenotyping/results/20260507_5_cluster_results.csv", index=False)
+df.to_csv("/home/martinez/flower_phenotyping/results/20260505_5_cluster_results.csv", index=False)
 
 print(df.head())
+
+# ========================
+# 11. CLUSTERING METRICS
+# ========================
+
+from sklearn.metrics import silhouette_score
+
+score = silhouette_score(X_scaled, clusters)
+
+print(score)
