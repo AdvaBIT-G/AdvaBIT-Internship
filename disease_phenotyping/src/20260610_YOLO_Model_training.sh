9b1b3c615583f9d5150520c91208d947117518fe
martinez <gema.martinez.sanchez@student.howest.be>
2026-06-11 10:52:36 +0200

    Change in the name of COCO and SAHI scripts

--- disease_phenotyping/src/20260610_YOLO_Model_training.sh
+++ disease_phenotyping/src/20260610_YOLO_Model_training.sh
@@ -13,8 +13,8 @@ COCO="${WORK}/coco_out"
 SAHI="${WORK}/coco_sliced"
 YOLO="${WORK}/yolo_dataset"
 
-COCO_SCRIPT="/home/martinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/src/20260610_jsons_to_coco.py"
-SAHI_SCRIPT="/home/martinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/src/20260610_YOLO_SAHI_Training.py"
+COCO_SCRIPT="/home/martinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/src/20260611_jsons_to_coco.py"
+SAHI_SCRIPT="/home/martinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/src/20260611_YOLO_SAHI_Training.py"
 
 log() { echo "[$(date +%H:%M:%S)] $*"; }
 die() { echo "[ERROR] $*" >&2; exit 1; }
