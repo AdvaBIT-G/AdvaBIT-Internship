e5447e127b8ab116cdcf10122e95803282e59b6b
AdvaBIT-G <gema.martinez.sanchez@student.howest.be>
2026-08-04 11:07:46 +0200

    Fixing bugs in the paths

--- flower_phenotyping/src/20260803_Autoencoder_Training.py
+++ flower_phenotyping/src/20260803_Autoencoder_Training.py
@@ -17,8 +17,8 @@ from autoencoder_arch import FlowerDataset, Autoencoder
 # =========================
 
 # Input data and output paths
-MASK_DIR = "/home/gmartinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/data/autoencoder/train_masks"
-CHECKPOINT_DIR = "/home/gmartinez/internship_howest/AdvaBIT-Internship/flower_phenotyping/models/autoencoder"
+MASK_DIR = "/home/gmartinez/AdvaBIT-Internship/flower_phenotyping/data/autoencoder/train_masks"
+CHECKPOINT_DIR = "/home/gmartinez/AdvaBIT-Internship/flower_phenotyping/models/autoencoder"
 FINAL_MODEL_PATH = os.path.join(CHECKPOINT_DIR, "autoencoder_final.pth")
 
 # Training parameters
